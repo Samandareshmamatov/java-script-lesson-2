2487660bfe5e276778c3bb46b990820b5f406939
Samandareshmamatov <samandareshmamatov1127@gmail.com>
2022-11-11 10:18:14 +0500

    Change js

--- taskHomework.js
+++ taskHomework.js
@@ -5,6 +5,12 @@ let dollar = 11000.34;
 let euro = 12354.03;
 let cost = 500 * dollar + 250 * dollar + 120 * euro;
 if ((yourMoney - cost) >= 0) {
+    document.write(`Borish-kelish samolyot bileti - $500;
+  Mehmonxona to’lovi (to’liq safar davomiyligi uchun) - $250;
+  Muzey va ko’ngilochar joylar uchun - 120 yevro;
+  Hozirda kurs : 
+  $1 = 11000.34 so’m
+  1 yevro = 12354.03 so’m`)
     document.write(`Sizning mablag'ingiz : ${yourMoney} so'm va ${yourMoney - cost} so'm ortiqcha pulingiz bor\n` + `<br><br>`);
     document.write(`Oq yo'l ${firstName} ✈️✈️`);
 } else {
